5c8f94b956db582daaeffdb4a7a46e7bc626d757
oreo1001 <ohsimon77@naver.com>
2024-06-25 11:25:36 +0900

    commentSlice 삭제

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import createWebStorage from 'redux-persist/lib/storage/createWebStorage'
 import persistedChatReducer from './slices/chatSlice'
 import persistedBoardReducer from './slices/boardSlice'
-import persistedCommentReducer from './slices/commentSlice'
 
 export const store = configureStore({
     reducer: {
         chat: persistedChatReducer,
         board: persistedBoardReducer,
-        comment: persistedCommentReducer,
     },
     //   devTools: process.env.NODE_ENV !== "production",
 })
